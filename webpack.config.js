7fc001e0b3e0ef1ce2c3441fcae6c6ef6ce5fd2e
jflayhart <ijoshg8@mac.com>
2019-09-18 22:46:22 -0500

    Fix path to assets

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 8000, // Convert images < 8kb to base64 strings
-            name: 'img/[hash]-[name].[ext]'
+            name: 'img/[name].[ext]'
           }
         }]
       }
