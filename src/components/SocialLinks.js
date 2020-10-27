20050e7479695a81c968a1e9d34e9602831f413f
Joshua Flayhart <joshuaflayhart@ip-192-168-1-38.ec2.internal>
2020-10-27 14:09:21 -0500

    Clean up

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -6,24 +6,22 @@ const styles = {
   },
   icon: {
     margin: '0 15px',
-    width: '40px',
-    height: '40px',
   },
 }
 
 const SocialLinks = () => (
   <div style={styles.container}>
     <div style={styles.icon}>
-      <a href="https://www.facebook.com/jjflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/facebook.svg" width="40" alt="facebook" aria-label="facebook" /></a>
+      <a href="https://www.facebook.com/jjflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/facebook.svg" width="40" height="40" alt="facebook" aria-label="facebook" /></a>
     </div>
     <div style={styles.icon}>
-      <a href="https://twitter.com/joshflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/twitter.svg" width="40" alt="twitter" aria-label="twitter" /></a>
+      <a href="https://twitter.com/joshflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/twitter.svg" width="40" height="40" alt="twitter" aria-label="twitter" /></a>
     </div>
     <div style={styles.icon}>
-      <a href="https://www.linkedin.com/in/jflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.svg" width="40" alt="linkedin" aria-label="linkedin" /></a>
+      <a href="https://www.linkedin.com/in/jflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.svg" width="40" height="40" alt="linkedin" aria-label="linkedin" /></a>
     </div>
     <div style={styles.icon}>
-      <a href="https://github.com/jflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/github.svg" width="40" alt="github" aria-label="github" /></a>
+      <a href="https://github.com/jflayhart" target="_blank" rel="noopener noreferrer"><img src="/images/github.svg" width="40" height="40" alt="github" aria-label="github" /></a>
     </div>
   </div>
 )
