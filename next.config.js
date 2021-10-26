e4032f01fe2b6d7f30991e598e05a722774d1d89
Josh Flayhart <jflayhart@users.noreply.github.com>
2021-10-26 17:43:22 -0500

    Add Service Worker (#10)
    * updated npm version
    
    * update gitignore
    
    * Add SW via workbox
    
    * Add SW via workbox
    
    * Convert _app to FC and register SW
    
    * fix formatting
    
    * Add manifest
    
    * Add manifest
    
    * Add SW to publich
    
    * exlucxde
    
    * add url prefix
    
    * bleh
    
    * wip
    
    * wip
    
    * wtf
    
    * wtf
    
    * wtf
    
    * wtf
    
    * add manifest
    
    * add manifest
    
    * test
    
    * fix a11y
    
    * include images it's fine

---
+++ next.config.js
@@ -0,0 +1,31 @@
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+const publicPath = 'public'
+
+module.exports = {
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      // Service Worker
+      // https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
+      // https://developers.google.com/web/tools/workbox/guides/generate-service-worker/webpack#adding_runtime_caching
+      config.plugins.push(
+        new WorkboxPlugin.GenerateSW({
+          swDest: path.join(publicPath, 'service-worker'),
+          modifyURLPrefix: {
+            '.next': '/_next',
+            'static/': '_next/static/',
+          },
+          exclude: [
+            'react-loadable-manifest.json',
+            'build-manifest.json',
+            '_buildManifest.js'
+          ],
+          skipWaiting: true,
+          clientsClaim: true,
+          cleanupOutdatedCaches: true
+        })
+      )
+    }
+    return config
+  }
+}
