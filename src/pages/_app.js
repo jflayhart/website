262efc5e4c8d257d0d741e76f229e995c2170299
Josh Flayhart <jflayhart@shipt.com>
2021-10-26 16:25:39 -0500

    fix formatting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,14 +9,14 @@ function MyApp({ Component, pageProps }) {
     if('serviceWorker' in navigator) {
       window.addEventListener('load', function () {
         navigator.serviceWorker.register('/service-worker.js').then(
-            function (registration) {
-              console.log('Service Worker registration successful with scope: ', registration.scope);
-            },
-            function (err) {
-              console.error('Service Worker registration failed: ', err);
-            }
-        );
-      });
+          function (registration) {
+            console.log('Service Worker registration successful with scope: ', registration.scope)
+          },
+          function (err) {
+            console.error('Service Worker registration failed: ', err)
+          }
+        )
+      })
     }
   }, [])
 
